cd9741411f61e1e267b4536e9811550e2ff6abde
sayl1th <saylith@icloud.com>
2020-07-30 19:00:39 +0200

    Updates

---
+++ precache-manifest.027e6b0798218022c4b441ce51a1581d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e099bf1ed3a451b00860702712c900",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "78ad3bd7dee5ae61c009",
+    "url": "/smart-brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "849ea0038c03a16945a6",
+    "url": "/smart-brain/static/css/main.6c41eac0.chunk.css"
+  },
+  {
+    "revision": "78ad3bd7dee5ae61c009",
+    "url": "/smart-brain/static/js/2.f8007f8a.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smart-brain/static/js/2.f8007f8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "849ea0038c03a16945a6",
+    "url": "/smart-brain/static/js/main.f5550657.chunk.js"
+  },
+  {
+    "revision": "5f284a9b7132a669a655",
+    "url": "/smart-brain/static/js/runtime-main.43daafc1.js"
+  }
+]);
